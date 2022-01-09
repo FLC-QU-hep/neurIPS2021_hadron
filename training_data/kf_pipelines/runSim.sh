2d2022938f1d0bb2c3dd4722295834cf0a3abae1
Engin Eren <engin.eren@desy.de>
2022-01-09 18:32:27 +0100

    adding lcgeo dir

--- training_data/kf_pipelines/runSim.sh
+++ training_data/kf_pipelines/runSim.sh
@@ -8,6 +8,6 @@ git clone --branch v02-01-pre02 https://github.com/iLCSoft/ILDConfig.git
 cp ../* ./ILDConfig/StandardConfig/production
 cd ./ILDConfig/StandardConfig/production
 
-echo "-- Running DDSim $(SIM_MODEL) ..."
-ddsim --outputFile ./pion-shower.slcio --compactFile ./compact/${SIM_MODEL}/${SIM_MODEL}.xml --steeringFile ddsim_steer_gun.py 
+echo "-- Running DDSim..."
+ddsim --outputFile ./pion-shower.slcio --compactFile $lcgeo_DIR/ILD/compact/${SIM_MODEL}/${SIM_MODEL}.xml --steeringFile ddsim_steer_gun.py 
 
